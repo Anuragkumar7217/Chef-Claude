bb30eeee0b358e17d7a84929190ab2ecd1295ecf
Anurag <anuragkumar7217@gmail.com>
2025-02-04 11:12:26 +0530

    comment3

--- src/components/ClaudeRecipe.jsx
+++ src/components/ClaudeRecipe.jsx
@@ -3,9 +3,8 @@ import ReactMarkdown from "react-markdown"
 export default function ClaudeRecipe(props){
     return(
         <section className="p-15 bg-gray-200 rounded-lg shadow-lg">
-            <h2 className="text-3xl font-medium">Chef Claude Recommends:</h2>
+            <h2 className="text-3xl font-medium">Chef Gemini Recommends:</h2>
             <div className="">
-                {/* <p>{props.recipe}</p> */}
                 <ReactMarkdown>{props.recipe}</ReactMarkdown>
             </div>            
        </section>
